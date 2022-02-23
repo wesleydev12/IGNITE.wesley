360be7a95ba4d964c774344a6afa4c3450339013
*Wesley <wesleydaybusi12@gmail.com>
2022-02-23 20:46:43 -0300

    'ERRO'

--- github-explorer/src/index.js
+++
@@ -1,4 +0,0 @@
-const user = {
-    name: 'Wesley'
-}
-console.log(user.adress?.street);
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+// import React from 'react'; não precisa mais
+import { render } from 'react-dom';
+import { App } from './App';
+
+render(<App />, document.getElementById('root'))
+
